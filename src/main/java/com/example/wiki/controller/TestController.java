e0a3ea41edd6241ff8c268f979e8b285599dd9b7
Satck <105479799+Satck@users.noreply.github.com>
2023-11-06 16:37:43 +0800

    2.6 开发hello world接口 访问地址：http://127.0.0.1:8080/hello

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,4 +1,22 @@
 package com.example.wiki.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
 public class TestController {
+
+    /**
+     * 常见的请求方式 GET PUT POST DELETE
+     * @return
+     */
+
+    @RequestMapping("/hello")
+    // 如果只是简单的用@RequestMapping注解 表示这个接口支持所有的请求方式
+    // 如果要制定某种方式的话 eg get请求 那么就用GetMapping（）
+    public String hello(){
+        return "Hello World";
+    }
 }
