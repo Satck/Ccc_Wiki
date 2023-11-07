9ff22eaa447aacdc6638ee03e5f81a6344eba022
Satck <105479799+Satck@users.noreply.github.com>
2023-11-07 11:57:06 +0800

    2.7 增加hello接口测试test.http

--- src/main/java/com/example/wiki/controller/TestController.java
+++ src/main/java/com/example/wiki/controller/TestController.java
@@ -1,6 +1,6 @@
 package com.example.wiki.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +16,13 @@ public class TestController {
     @RequestMapping("/hello")
     // 如果只是简单的用@RequestMapping注解 表示这个接口支持所有的请求方式
     // 如果要制定某种方式的话 eg get请求 那么就用GetMapping（）
+    // PostMapping 不可以使用浏览器来测试
     public String hello(){
         return "Hello World";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world postMapping : " + name ;
     }
 }
