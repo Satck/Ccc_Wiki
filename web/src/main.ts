a467bb4df568fa25e65bf30702d487dc472137b2
Satck <105479799+Satck@users.noreply.github.com>
2023-11-13 17:46:39 +0800

    4.5 集成Ant Design Vue 加入按钮示例

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import  Antd from 'ant-design-vue'
+import  'ant-design-vue/dist/antd.css'
+//需要哪些组件 就可以在这个里面进行使用
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(Antd).mount('#app')
