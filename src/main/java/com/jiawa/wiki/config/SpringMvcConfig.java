46d03f313c812a9cf22b21dfc77c599055e381d7
Satck <105479799+Satck@users.noreply.github.com>
2023-11-16 08:47:30 +0800

    5.9 增加AOP 打印耗时接口 请求参数和返回参数 去掉过滤器 拦截器

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,21 @@
-package com.jiawa.wiki.config;
-
-
-import com.jiawa.wiki.interceptor.LogInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class SpringMvcConfig implements WebMvcConfigurer {
-
-    @Autowired
-    LogInterceptor logInterceptor;
-
-    //拦截器先结束 过滤器才结束
-    public void addInterceptor(InterceptorRegistry registry){
-        registry.addInterceptor(logInterceptor)
-                .addPathPatterns("/**").excludePathPatterns("/login");
-    }
-}
+//package com.jiawa.wiki.config;
+//
+//
+//import com.jiawa.wiki.interceptor.LogInterceptor;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class SpringMvcConfig implements WebMvcConfigurer {
+//
+//    @Autowired
+//    LogInterceptor logInterceptor;
+//
+//    //拦截器先结束 过滤器才结束
+//    public void addInterceptor(InterceptorRegistry registry){
+//        registry.addInterceptor(logInterceptor)
+//                .addPathPatterns("/**").excludePathPatterns("/login");
+//    }
+//}
