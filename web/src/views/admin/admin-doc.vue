2765d7dcd06d76ef966beefd3e51e3746e289aeb
Satck <105479799+Satck@users.noreply.github.com>
2023-12-01 21:06:04 +0800

    8.5 增加页面参数传递完成新增文档功能

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -78,14 +78,16 @@ import { defineComponent, onMounted, ref } from 'vue';
 import axios from 'axios';
 import {message} from "ant-design-vue";
 import { Tool } from '@/util/tool';
+import {useRoute} from "vue-router";
 
 export default defineComponent({
   name: 'AdminDoc',
   setup(){
-  const param = ref();
-  param.value = {};
-  const docs = ref();
-  const loading = ref(false);
+      const route = useRoute();
+      const param = ref();
+      param.value = {};
+      const docs = ref();
+      const loading = ref(false);
     const columns = [
       {
         title: '封面',
@@ -186,7 +188,9 @@ export default defineComponent({
      */
     const add = () => {
       modalVisible.value = true ;
-      doc.value = {};
+      doc.value = {
+        ebookId: route.query.ebookId
+      };
 
       treeSelectData.value =Tool.copy(level1.value);
 
