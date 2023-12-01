e366a313d1d2f5d84c449fbc492151014783187f
Satck <105479799+Satck@users.noreply.github.com>
2023-12-01 20:56:01 +0800

    8.4 使用递归算法 将树形数据中 当前目标节点及其子孙节点设置成disabled 解决编辑保存后 重新编辑时 数据不更新的问题 为树形选择增加一个”无“选择 用来新增一级节点

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -42,7 +42,7 @@
             </a-popconfirm>
           </a-space>
         </template>
-      </a-table>0
+      </a-table>
     </a-layout-content>
   </a-layout>
   <a-modal
@@ -55,16 +55,17 @@
       <a-form-item label="名称">
         <a-input v-model:value="doc.name" />
       </a-form-item>
-      <a-form-item label="父文档">
-        <a-select
-          v-model:value = "doc.parent"
-          ref = "select"
+      <a-form-item  label="父文档">
+        <a-tree-select
+            v-model:value="doc.parent"
+            style="width: 100%"
+            :dropdown-style="{ maxHeight: '400px', overflow: 'auto' }"
+            :tree-data="treeSelectData"
+            placeholder="请选择父文档"
+            tree-default-expand-all
+            :replaceFields="{title: 'name', key: 'id', value: 'id'}"
         >
-          <a-select-option value = "0"> 无</a-select-option>
-          <a-select-option v-for="c in level1" :key = "c.id" :value ="c.id" :disabled="doc.id===c.id">
-            {{c.name}}
-          </a-select-option>
-        </a-select>
+        </a-tree-select>
       </a-form-item>
       <a-form-item label="顺序">
         <a-input v-model:value="doc.sort" />
@@ -146,7 +147,10 @@ export default defineComponent({
       });
     };
       //--------表单------------
-    const doc = ref();
+    // 因为树选择组建的属性状态 会随着当前编辑的节点而变化 所以单独声明一个响应式变量
+    const treeSelectData = ref();
+    treeSelectData.value = [];
+    const doc = ref({});
     const modalVisible = ref(false);
     const modalLoading = ref(false);
     const handleModalOk = () => {
@@ -169,6 +173,13 @@ export default defineComponent({
     const edit = (record:any) => {
       modalVisible.value = true ;
       doc.value = Tool.copy(record);
+
+      // 不能选择当前节点及所有的子孙节点 作为父节点 会使树断开
+      treeSelectData.value = Tool.copy(level1.value);
+      setDisable(treeSelectData.value,record.id)
+
+      // 为选择树 添加一个”无“
+      treeSelectData.value.unshift({id:0,name:'无'});
     };
     /**
      * 新增
@@ -176,6 +187,11 @@ export default defineComponent({
     const add = () => {
       modalVisible.value = true ;
       doc.value = {};
+
+      treeSelectData.value =Tool.copy(level1.value);
+
+      // 为选择树添加一个”无“
+      treeSelectData.value.unshift({id:0,name:'无'});
     };
 
     const handleDelete=(id : number) => {
@@ -186,6 +202,37 @@ export default defineComponent({
           handleQuery();
         }
       });
+    };
+
+    /**
+     * 将某节点及其子孙节点全部置为disabled
+     */
+    const setDisable = (treeSelectData: any, id: any) => {
+      // console.log(treeSelectData, id);
+      // 遍历数组，即遍历某一层节点
+      for (let i = 0; i < treeSelectData.length; i++) {
+        const node = treeSelectData[i];
+        if (node.id === id) {
+          // 如果当前节点就是目标节点
+          console.log("disabled", node);
+          // 将目标节点设置为disabled
+          node.disabled = true;
+
+          // 遍历所有子节点，将所有子节点全部都加上disabled
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            for (let j = 0; j < children.length; j++) {
+              setDisable(children, children[j].id)
+            }
+          }
+        } else {
+          // 如果当前节点不是目标节点，则到其子节点再找找看。
+          const children = node.children;
+          if (Tool.isNotEmpty(children)) {
+            setDisable(children, id);
+          }
+        }
+      }
     };
 
 
@@ -210,6 +257,8 @@ export default defineComponent({
       handleModalOk,
       handleDelete,
       handleQuery,
+
+      treeSelectData
     }
   }
 })
