5ddab5ec603fd92f2e2f7b9b3770f9f81d8fbb7d
Satck <105479799+Satck@users.noreply.github.com>
2023-12-05 18:56:55 +0800

    9.11  前后端增加登录拦截 修复BUG 前后端存储token统一用String类型

--- src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
+++ src/main/java/com/jiawa/wiki/config/SpringMvcConfig.java
@@ -1,21 +1,41 @@
-//package com.jiawa.wiki.config;
-//
-//
-//import com.jiawa.wiki.interceptor.LogInterceptor;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class SpringMvcConfig implements WebMvcConfigurer {
-//
-//    @Autowired
-//    LogInterceptor logInterceptor;
-//
-//    //拦截器先结束 过滤器才结束
-//    public void addInterceptor(InterceptorRegistry registry){
-//        registry.addInterceptor(logInterceptor)
-//                .addPathPatterns("/**").excludePathPatterns("/login");
-//    }
-//}
+package com.jiawa.wiki.config;
+
+import com.jiawa.wiki.interceptor.ActionInterceptor;
+import com.jiawa.wiki.interceptor.LoginInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.annotation.Resource;
+
+@Configuration
+public class SpringMvcConfig implements WebMvcConfigurer {
+
+    @Resource
+    LoginInterceptor loginInterceptor;
+
+    @Resource
+    ActionInterceptor actionInterceptor;
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns(
+                        "/test/**",
+                        "/redis/**",
+                        "/user/login",
+                        "/category/all",
+                        "/ebook/list",
+                        "/doc/all/**",
+                        "/doc/vote/**",
+                        "/doc/find-content/**",
+                        "/ebook-snapshot/**"
+                );
+
+        registry.addInterceptor(actionInterceptor)
+                .addPathPatterns(
+                        "/*/save",
+                        "/*/delete/**",
+                        "/*/reset-password");
+    }
+}
